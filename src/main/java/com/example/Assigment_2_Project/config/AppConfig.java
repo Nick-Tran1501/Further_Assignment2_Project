58aa0da7e95ecc6fd21ca143a93b0bc32648f88a
KhoiNguyen-281 <s3759804@rmit.edu.vn>
2022-04-27 21:57:25 +0700

    Create AppConfig

---
+++ src/main/java/com/example/Assigment_2_Project/config/AppConfig.java
@@ -0,0 +1,62 @@
+package com.example.Assigment_2_Project.config;
+
+import com.example.Assigment_2_Project.model.Booking;
+import org.hibernate.SessionFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.awt.print.Book;
+import java.util.Properties;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@Configuration
+@EnableTransactionManagement
+@EnableWebMvc
+public class AppConfig {
+
+    @Bean
+    public Booking booking() {
+        return new Booking();
+    }
+
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactoryBean() {
+        Properties properties = new Properties();
+        //For Postgresql
+        properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        //For mysql
+        //properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+        properties.put("hibernate.show_sql", true);
+        properties.put("hibernate.hbm2ddl.auto", "update");
+
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+
+        sessionFactoryBean.setPackagesToScan("com/example/Assigment_2_Project/model");
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/Assignment_2");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("khoinguyen28100"); //REmember to modify
+
+        sessionFactoryBean.setDataSource(dataSource);
+        sessionFactoryBean.setHibernateProperties(properties);
+
+        return sessionFactoryBean;
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+        HibernateTransactionManager tx = new HibernateTransactionManager(sessionFactory);
+        return tx;
+    }
+}
+
